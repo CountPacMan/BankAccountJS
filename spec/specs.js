6db5541894fa31f85255de5ef7a126344a7dfd42
Daniel Toader & Cynthia Wilhelm <student@epicodus.com>
2015-04-15 09:38:28 -0700

    Create function and test for deposits

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe('BankAccount', function() {
-  it("", function() {
-    expect().to.equal();
+  describe('deposit()', function() {
+    it("deposits $10 to the account", function() {
+      var testAccount = Object.create(BankAccount);
+      testAccount.deposit(10);
+      expect(testAccount.balance).to.equal(10);
+    });
   });
 });
